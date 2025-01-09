dc3cc54b8c0ac9afde93f46af8fbc1b4f6e0831a
LazizbekShuxratov <shuxratovlazizbek813@gmail.com>
2025-01-09 19:15:25 +0500

    start

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Product = () => {
   return (
        <div>
-            Product page container
+            Product page container Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam nulla exercitationem iste ducimus impedit possimus repellendus error veritatis, deleniti velit alias hic laborum, minima consequuntur voluptate dolores, fuga doloremque temporibus perferendis rerum? A eos quaerat, unde culpa reiciendis alias ipsa molestiae, adipisci deleniti voluptatum quidem excepturi quod quis maiores porro molestias laudantium at explicabo! Repudiandae exercitationem nostrum consequuntur vitae voluptas. Perferendis voluptatibus sequi fuga a ullam fugiat est, reiciendis aut eaque dolore cupiditate soluta exercitationem dolor optio non! Obcaecati minus magni quasi nesciunt enim ullam, perspiciatis animi doloribus architecto commodi voluptates esse, explicabo consequuntur non, corrupti dolorum? Modi, voluptas labore.
     </div>
   )
 }
