090f3684357d53ee7fa28431957772ecef655bd1
AzizbekTohirov <aziztohirov796@gmail.com>
2025-01-09 17:15:53 +0500

    Azizbek

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,11 +3,7 @@ import React from 'react'
 const About = () => {
   return (
        <div>
-            About
-         About container
-
-
-         
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti sit quam modi corporis maiores eaque, vitae iusto deleniti distinctio beatae explicabo dolor ullam debitis ab. Odio voluptatem voluptate consequatur dolorum eveniet pariatur tempora asperiores quidem. Dicta aliquid dolor, saepe non ad debitis optio quisquam, nostrum hic tempore at, tenetur aperiam provident! Id, delectus alias! At facere qui sunt tempore quo veniam temporibus libero. Exercitationem nemo aut eligendi earum amet sint dicta consectetur reprehenderit facere quisquam distinctio, numquam possimus impedit dolor fugiat nostrum ipsa nam! Nobis molestiae quasi ad placeat sequi dignissimos, cupiditate, ex consectetur iste architecto accusantium! Iusto, autem corrupti.   
     </div>
   )
 }
